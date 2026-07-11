d002fbc40315449f0d87eb31e7d59376b45ea7b6
windyzhu3 <156416967+windyzhu3@users.noreply.github.com>
2026-07-11 21:32:32 +0800

    refactor: extract finance data tables

---
+++ ruoyi-ui/src/views/finance/components/FinanceDataTables.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FinanceDataTables',
+  props: {
+    mode: { type: String, required: true },
+    loading: Boolean,
+    list: { type: Array, default: () => [] },
+    controlSize: { type: String, default: 'small' },
+    dictOptions: { type: Object, default: () => ({}) },
+    formatMoney: { type: Function, required: true },
+    field: { type: Function, required: true },
+    canCollect: { type: Function, required: true },
+    canInvoice: { type: Function, required: true },
+    openCaseFinance: { type: Function, required: true },
+    openPayment: { type: Function, required: true },
+    openReject: { type: Function, required: true },
+    openInvoice: { type: Function, required: true },
+    openExpense: { type: Function, required: true }
+  }
+}
+</script>
+
